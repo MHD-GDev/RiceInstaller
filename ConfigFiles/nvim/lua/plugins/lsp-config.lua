8397cbaed5180e0e25bd80b7a340d52663faee22
mhd <mhdworking1@gmail.com>
2025-11-07 17:10:29 +0000

    Update

--- ConfigFiles/nvim/lua/plugins/lsp-config.lua
+++ ConfigFiles/nvim/lua/plugins/lsp-config.lua
@@ -1,48 +1,40 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    cmd = "Mason",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    opts = {
-      ensure_installed = {
-        "bashls",
-        "lua_ls",
-        "cssls",
-        "csharp_ls",
-        "clangd",
-        "marksman",
-        "rust_analyzer",
-      },
-      auto_install = true,
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		cmd = "Mason",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			ensure_installed = {},
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      -- new API
-      vim.lsp.config("bashls", { capabilities = capabilities })
-      vim.lsp.config("cssls", { capabilities = capabilities })
-      vim.lsp.config("lua_ls", { capabilities = capabilities })
-      vim.lsp.config("csharp_ls", { capabilities = capabilities })
-      vim.lsp.config("clangd", { capabilities = capabilities })
-      vim.lsp.config("marksman", { capabilities = capabilities })
-      vim.lsp.config("rust_analyzer", { capabilities = capabilities })
+			-- new API
+			vim.lsp.config("bashls", { capabilities = capabilities })
+			vim.lsp.config("cssls", { capabilities = capabilities })
+			vim.lsp.config("lua_ls", { capabilities = capabilities })
+			vim.lsp.config("csharp_ls", { capabilities = capabilities })
+			vim.lsp.config("clangd", { capabilities = capabilities })
+			vim.lsp.config("marksman", { capabilities = capabilities })
+			vim.lsp.config("rust_analyzer", { capabilities = capabilities })
 
-      -- keymaps
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Definitions" })
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "References" })
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
-      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format)
-    end,
-  },
+			-- keymaps
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Definitions" })
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "References" })
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format)
+		end,
+	},
 }
